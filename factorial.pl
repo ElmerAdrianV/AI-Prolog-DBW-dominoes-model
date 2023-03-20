8ad8d2a389b6e570bed33fb4ed06294aeedb33fc
elmeradrianv <elmeradrianv@outlook.com>
2023-03-20 15:25:22 -0600

    Agregando Factorial dinamico

---
+++ factorial.pl
@@ -0,0 +1,15 @@
+:- dynamic
+    cons_factorial/2. %Nos dice si tenemos cierta ficha.
+
+cons_factorial(0,1) :- !.
+
+factorial(N,Resp) :-
+    cons_factorial(N,Resp),! ;
+    A is N-1,
+    (cons_factorial(A,_) ->
+        cons_factorial(A,Fact_n1)
+        ;
+        factorial(A, Fact_n1)
+    ),
+    Resp is N*Fact_n1,
+    asserta(cons_factorial(N,Resp)).
