b0d5937766ee040eb3f45b9ec25b2c155a5b50c4
elmeradrianv <elmeradrianv@outlook.com>
2023-03-22 15:15:01 -0600

    Salvando borrador

--- alphabeta.pl
+++ alphabeta.pl
@@ -1,2 +1,32 @@
+dynamic:-
+    alpha/1,
+    iteracion/1,
+    beta/1.
+
+repite.
+repite:-
+	repite.
+
 carga_archivos:-
     [funcion_heuristica].
+
+alphabeta(State,Ficha,Depth,Alpha,Beta, MaxiPlayer, ValHeur):-
+    (   finish_search(Depth,State) ->
+            funcion_heuristica(State,Ficha, ValHeur);
+        (   MaxiPlayer =:=1 ->
+            ( continua(ListaManoDBW) -> 
+                ;
+                
+            )
+            ;
+        )
+    )
+finish_search(Depth,State):-
+    Depth=:=0,!;
+    terminal_state(State).
+continua(Lista):-
+    alpha(Alpha),
+    beta(Beta),
+    Beta > Alpha,!;
+    length(Lista,Len),
+    Len =\= 0. 
